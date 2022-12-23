297234699b42c0a745779fed2df0762c053ea3b3
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2022-12-23 09:03:32 +0200

    handled empty state

--- tasks.js
+++ tasks.js
@@ -1,60 +1,55 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n' || text === 'exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
+    text =text.replace('\n',' ');
     quit();
   }
-  else if(text === 'hello\n'){
-    hello();
-  }
-  else if (text === 'help\n')
+  else if (text.startsWith('hello')|| text ===("hello\n")) {
+   hello(text)
 
-  {help();
-  }
-  else{
+  } else if (text === "help\n") {
+    help();
+  } else {
     unknownCommand(text);
   }
 }
 
-
-/** 
-* @param  {string} text1
-* @returns {void}
-*/
-
+/**
+ * @param  {string} text1
+ * @returns {void}
+ */
 
 /**
  * prints "unknown command"
@@ -63,41 +58,53 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
-}
+// function hello() {
+//   console.log("hello!");
+// }
 
+/**
+ * Says hello
+ *
+ * @returns {void}
+ */
+function hello(text) {
+  //  var txt = text.substring()
+  console.log(text.trim()+'!');
+}
+/**
+ * hello !
+ *
+ * @returns {void}
+ */
 
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function quit(){
-  console.log('Quitting now, goodbye!')
+function quit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
- 
 }
+
 /**
  * help:Lists all the commands
  *
  * @returns {void}
  */
-function help(){
-  console.log('hello:Says hello\nquit:Exits the app\nquit:Exits the app\n');
-
+function help() {
+  console.log("hello:Says hello\nquit:Exits the app\nquit:Exits the app\n");
 }
 
 // The following line starts the application
-startApp("Fatina Elomar")
+startApp("Fatina Elomar");
 // add exit
